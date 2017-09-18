370e0b835e884ef83f9299b17e1a29d2c62209e9
jinshuaier <470401910@qq.com>
2017-09-18 10:40:52 +0800

    Initial Commit

---
+++ 全选和单选/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  全选和单选
+//
+//  Created by 胡高广 on 2017/9/18.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 全选和单选/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  全选和单选
+//
+//  Created by 胡高广 on 2017/9/18.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
